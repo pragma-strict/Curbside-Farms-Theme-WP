45c014b8efa2624e6e1e61017dc990955deb2e71
Ian <ianrdejong@gmail.com>
2021-10-14 01:04:25 -0700

    Add 'vision' and 'our team' to front page

--- front-page.php
+++ front-page.php
@@ -11,6 +11,59 @@
       </div>
 </div>
 
+<div class="spacer"></div>
+
+<div class="about-page">
+    <div class="container">
+        <h1>We believe homegrown food is the future</h1>
+        <p>If you've ever eaten vegetables straight from the garden you know what we're talking about. Once you go fresh, it's hard to go back. Incredible flavour, though, is only part of the story. </p>
+        <div class="about-section">
+            <div class="icon">
+                <img src="http://curbsidefarmsca.local/wp-content/uploads/2021/04/factory.png" alt="" width="254" height="300" class="alignnone size-medium wp-image-136" />
+            </div>
+            <h4>Reduce GHG emissions</h4>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio, ipsum. Inventore, doloribus! Magni, praesentium distinctio. Quisquam delectus quaerat maxime sapiente exercitationem, vel alias nobis? Eius veritatis ut dignissimos at iste!</p>
+        </div>
+        <div class="about-section">
+            <div class="icon">
+                <img src="http://curbsidefarmsca.local/wp-content/uploads/2021/04/sapling.png" alt="" width="254" height="300" class="alignnone size-medium wp-image-136" />
+            </div>
+            <h4>Build connection to the land</h4>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio, ipsum. Inventore, doloribus! Magni, praesentium distinctio. Quisquam delectus quaerat maxime sapiente exercitationem, vel alias nobis? Eius veritatis ut dignissimos at iste!</p>
+        </div>
+        <div class="about-section">
+            <div class="icon">
+                <img src="http://curbsidefarmsca.local/wp-content/uploads/2021/04/forest-1.png" alt="" width="254" height="300" class="alignnone size-medium wp-image-136" />
+            </div>
+            <h4>Access more nutrients</h4>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio, ipsum. Inventore, doloribus! Magni, praesentium distinctio. Quisquam delectus quaerat maxime sapiente exercitationem, vel alias nobis? Eius veritatis ut dignissimos at iste!</p>
+        </div>
+        <hr>
+        <div>
+            <h1>In the long term</h1>
+            <p>We think people could work together better if they had the right tools to do so. We'd like to help build a place for people to build the communities they want using a co-operative model.</p>
+        </div>
+    </div>
+</div>
+
+<div class="spacer"></div>
+
+<div class="about-page">
+    <div class="container">
+        <h1>Our team</h1>
+        <p>We began as a club at the University of Victoria.</p>
+        <div class="about-section">
+            <div class="icon">
+                <img src="http://curbsidefarmsca.local/wp-content/uploads/2020/07/vlcsnap-2020-07-06-17h40m05s156.png" alt="" width="250" height="250" class="alignnone size-medium wp-image-136" />
+            </div>
+            <h4>Eric Buhne</h4>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio, ipsum. Inventore, doloribus! Magni, praesentium distinctio. Quisquam delectus quaerat maxime sapiente exercitationem, vel alias nobis? Eius veritatis ut dignissimos at iste!</p>
+        </div>
+    </div>
+</div>
+
+
+
 <script src="<?php echo( get_theme_root_uri( ) . "/curbside-farms/js/image-fill.js" ); ?>" type="module"></script>
 
 <?php get_footer(); ?>
